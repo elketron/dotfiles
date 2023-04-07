e10f86ecec090f54d6f6a5115ddf02a22d3403e9
elketron <odmar.goyvaerts@protonmail.com>
2023-04-07 10:40:01 +0200

    stuff added

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -10,31 +10,19 @@ capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- lsp config
 
 lsp.pyright.setup {
-  on_attach = on_attach,
   capabilities = capabilities,
 }
---lsp.pylsp.setup{}
 lsp.clangd.setup {
-  on_attach = on_attach,
   capabilities = capabilities,
 }
 lsp.html.setup {
-  on_attach = on_attach,
   capabilities = capabilities,
 }
 lsp.tsserver.setup { capabilities = capabilities }
 lsp.angularls.setup { capabilities = capabilities }
+lsp.tailwindcss.setup { capabilities = capabilities }
 lsp.cssls.setup { capabilities = capabilities }
 lsp.csharp_ls.setup { capabilities = capabilities }
---omnisharp_dll = vim.fn.expand("$HOME/omnisharp/OmniSharp.dll")
---lsp.omnisharp.setup {
---  capabilities = capabilities,
---  cmd = { "omnisharp" },
---  filetypes = { "cs", "vb", "razor" },
---  enable_roslyn_analyzers = true,
---  enable_import_completion = true,
---  organize_imports_on_format = true,
---}
 lsp.arduino_language_server.setup({
   cmd = {
     "arduino-language-server",
@@ -44,22 +32,7 @@ lsp.arduino_language_server.setup({
   capabilities = capabilities
 })
 lsp.sqlls.setup { capabilities = capabilities }
--- lsp.efm.setup{
---   init_options = {documentFormatting = true},
---   settings = {
---     rootMarkers = {".git/"},
---     languages = {
---       lua = {formatCommand = "lua-format -i", formatStdin = true},
---       python = {completion = true, codeAction = true}
---     }
---   },
---   filetypes = { 'python', 'lua'}
---}
-
-lsp.vuels.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+lsp.volar.setup { capabilities = capabilities }
 lsp.bashls.setup { capabilities = capabilities }
 lsp.rust_analyzer.setup { capabilities = capabilities }
 lsp.sumneko_lua.setup({
@@ -72,16 +45,6 @@ lsp.sumneko_lua.setup({
   }
 })
 
--- omnisharp
-
-local pid = vim.fn.getpid()
---local omnisharp_bin = "/usr/bin/omnisharp"
-
---require'lspconfig'.omnisharp.setup{
---  cmd = {omnisharp_bin},
---  enable_roslyn_analyzers = false,
---  enable_import_completion = false,
---}
 function PrintDiagnostics(opts, bufnr, line_nr, client_id)
   opts = opts or {}
 
