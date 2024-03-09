519d9b12df514be61c915d075cce90ca51add18f
elketron <odmar.goyvaerts@protonmail.com>
2024-03-09 15:36:29 +0100

    changes

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -56,16 +56,15 @@ return {
 	},
 	'VidocqH/lsp-lens.nvim',
 	{
-		"glepnir/lspsaga.nvim",
-		event = "LspAttach",
-		config = function()
-			require("lspsaga").setup({})
-		end,
-		dependencies = {
-			{ "nvim-tree/nvim-web-devicons" },
-			--Please make sure you install markdown and markdown_inline parser
-			{ "nvim-treesitter/nvim-treesitter" }
-		}
+		'nvimdev/lspsaga.nvim',
+		event = 'LspAttach',
+    config = function()
+        require('lspsaga').setup({})
+    end,
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter', -- optional
+        'nvim-tree/nvim-web-devicons'     -- optional
+    }
 	},
 	{ 'joeveiga/ng.nvim' },
 
