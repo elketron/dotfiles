88a566197d721365b22b0e640fa616c8b152c9d6
elketron <elketron1@proton.me>
2024-05-27 08:55:24 +0200

    Revert "restarting"
    This reverts commit feea2cb1bc8eafba4766b6d414c1429feda3bea4.

--- nvim/after/queries/rust/injections.scm
+++
@@ -1,7 +0,0 @@
-;; extends
-
-((
-  (raw_string_literal) @constant
-  (#match? @constant "(SELECT|select|insert|INSERT|update|UPDATE|delete|DELETE|create|CREATE|alter|ALTER).*")
-)@injection.content (#set! injection.language "sql"))
-
